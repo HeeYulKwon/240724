b1618e0959f74e855e9e7607a57a507cef6448bf
HeeYulKwon <s240057@baeksin.ms.kr>
2024-07-24 11:01:16 +0900

    Update main.py
    나의 첫 커밋

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('하하핳')
+st.title('쌈@뽕')
 name = st.text_input('이름 입력해! : ')
 menu = st.selectbox('좋아하는 음식 선택해!', ['망고빙수', '김치볶음밥', '회오리감자'])
 if st.button('인사말 생성') :
