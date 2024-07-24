1f4d76d0c0e0fbe8bda86e455ffa630a85686a61
HeeYulKwon <s240057@baeksin.ms.kr>
2024-07-24 11:04:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title('쌈@뽕')
 name = st.text_input('이름 입력해! : ')
 menu = st.selectbox('좋아하는 음식 선택해!', ['망고빙수', '김치볶음밥', '회오리감자'])
-if st.button('인사말 생성') :
-st.write(f'안녕 {name}! 너는 {menu}를 좋아하는구나!!멋찌다')
+if st.button == ('인사말 생성') :
+  st.write(f'안녕 {name}! 너는 {menu}를 좋아하는구나!!멋찌다')
